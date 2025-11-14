ba38ce682ef6c64be1daa5cdd8a3ab2fed6c8eb2
Sandeep Kumar Murmu <90225941+sandeepKumarMurmu@users.noreply.github.com>
2025-11-14 17:52:40 +0530

    Refactor EmailLogsController to handle email sending

--- src/modules/email-logs/email-logs.controller.ts
+++ src/modules/email-logs/email-logs.controller.ts
@@ -1,35 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Logger, Post, Query } from '@nestjs/common';
 import { EmailLogsService } from './email-logs.service';
+import { GetEmailsDto, SendMailDto } from './email.dto';
 
 @Controller()
 export class EmailLogsController {
+  private readonly logger = new Logger(EmailLogsController.name);
   constructor(private readonly emailLogsService: EmailLogsService) {}
 
-  // @Post()
-  // create(@Body() createEmailLogDto: CreateEmailLogDto) {
-  //   return this.emailLogsService.create(createEmailLogDto);
-  // }
-
-  @Get()
-  findAll() {
-    return this.emailLogsService.findAll();
+  @Post()
+  async sendMail(@Body() body: SendMailDto) {
+    try {
+      this.logger.log('Sending email with data:', JSON.stringify(body));
+      return this.emailLogsService.sendMail(body);
+    } catch (error) {
+      this.logger.error('Error sending mail:', error.message);
+      return { error: 'Failed to send email', details: error.message };
+    }
   }
 
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.emailLogsService.findOne(+id);
-  // }
-
-  // @Patch(':id')
-  // update(
-  //   @Param('id') id: string,
-  //   @Body() updateEmailLogDto: UpdateEmailLogDto,
-  // ) {
-  //   return this.emailLogsService.update(+id, updateEmailLogDto);
-  // }
-
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.emailLogsService.remove(+id);
-  // }
+  @Get()
+  async getEmailLogs(@Query() query: GetEmailsDto) {
+    return await this.emailLogsService.getEmailLogs(query);
+  }
 }
