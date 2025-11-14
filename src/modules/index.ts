454f5eb8508fc3821ab209ccc30c9301ecd5a958
Sandeep Kumar Murmu <90225941+sandeepKumarMurmu@users.noreply.github.com>
2025-11-14 17:56:50 +0530

    Add FileUploadModule to MainModules

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,16 +1,21 @@
 import { Module } from '@nestjs/common';
 import { RouterModule } from '@nestjs/core';
 import { EmailLogsModule } from './email-logs/email-logs.module';
+import { FileUploadModule } from './file-upload/file-upload.module';
 
 @Module({
   imports: [
     RouterModule.register([
       {
-        path: 'email',
-        children: [{ path: 'logs', module: EmailLogsModule }],
+        path: '',
+        children: [
+          { path: 'email', module: EmailLogsModule },
+          { path: 'upload', module: FileUploadModule },
+        ],
       },
     ]),
     EmailLogsModule,
+    FileUploadModule,
   ],
 })
 export class MainModules {}
