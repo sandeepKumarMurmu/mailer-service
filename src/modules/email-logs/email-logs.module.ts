e46e22cce980976ba81e01151608288082ce4a8b
Sandeep Kumar Murmu <90225941+sandeepKumarMurmu@users.noreply.github.com>
2025-11-14 17:53:02 +0530

    Add Mongoose integration for EmailLog schema

--- src/modules/email-logs/email-logs.module.ts
+++ src/modules/email-logs/email-logs.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { EmailLogsService } from './email-logs.service';
 import { EmailLogsController } from './email-logs.controller';
+import { EmailLog, EmailLogSchema } from '../../config/entity/email-log.schema';
 
 @Module({
   controllers: [EmailLogsController],
+  imports: [
+    MongooseModule.forFeature([
+      { name: EmailLog.name, schema: EmailLogSchema },
+    ]),
+  ],
   providers: [EmailLogsService],
+  exports: [EmailLogsService],
 })
 export class EmailLogsModule {}
